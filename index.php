07142916832c8b71365979cd54f873bd46ca06f4
irunn1412 <553241091@student.smktelkom-lpg.sch.id>
2025-08-29 15:03:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+?>
+<h2>Selamat Datang di Website irunn</h2>
+<p>Ini adalah halaman utama (Home). website ini di buat untuk mengenal irun lebih jauh yahuue.</p>
+<?php
+require 'footer.php';
+?>
