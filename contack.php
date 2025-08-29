971aa87ec589d5503bb06dfb46d9ce324e8f0917
irunn1412 <553241091@student.smktelkom-lpg.sch.id>
2025-08-29 15:02:02 +0700

    Add files via upload

---
+++ contack.php
@@ -0,0 +1,16 @@
+<?php
+include 'header.php';
+?>
+<h2>dm aku aja oke</h2>
+<form>
+    <label>Nama:Khoirun Nisya</label><br>
+    <input type="text" name="irunn"><br><br>
+
+    <label>Pesan:</label><br>
+    <textarea name="pesan"></textarea><br><br>
+
+    <button type="submit">Kirim</button>
+</form>
+<?php
+require 'footer.php';
+?>
